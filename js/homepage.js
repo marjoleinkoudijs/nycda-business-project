db7d5bf198e3870b798a74465e614f1165eb60fa
Marjolein Koudijs <koudijs.marjolein@gmail.com>
2017-03-09 13:24:14 +0100

    Image slider split text from images to support responsiveness

--- js/homepage.js
+++ js/homepage.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
 
-	var images = ["url(images/slide1.jpg)","url(images/slide2.jpg)","url(images/slide3.jpg)","url(images/slide4.jpg)"]
+	var text = ['We Are Special Flowers','From All The Daily Needs','To Special Occasions','You Can Rely On Us']
+	var images = ['url(images/slide1.jpg)','url(images/slide2.jpg)','url(images/slide3.jpg)','url(images/slide4.jpg)']
 	var numImages = images.length;
 	var index = 0;
 
 	function carousel() {
-    	$('#sectieSlide').css("background-image", images[index]);  
-    	$('#sectieSlide').css("background-size", "cover"); 
-    	$('#sectieSlide').css("background-position", "center center");   
+		$('#titleSlide').html(text[index]);  
+    	$('#sectieSlide').css('background-image', images[index]);  
+    	$('#sectieSlide').css('background-size', 'cover'); 
+    	$('#sectieSlide').css('background-position', 'center center');   
     	index = (index + 1) % numImages;
 	}
 
-	timer = setInterval(carousel, 2000);
+	timer = setInterval(carousel, 4000);
 });
